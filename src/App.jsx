7bcd1da221a7f81fc1309c95044da42ec5765283
ToniOswin <toni.bgae@gmail.com>
2022-03-30 12:36:10 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,56 +8,95 @@ export default function App(props) {
       <div className="mt-5">
         <div className="row align-items-center my-5 gy-2 text-center flex-column-reverse flex-md-row">
           <div className="col">
-            <img className="mw-100" src="https://via.placeholder.com/500x400" />
+            <img className="mw-100 shadow" src="/src/Images/mirrors.jpg" />
           </div>
           <div className="col">
-            <h2>Mavi's by dunamis</h2>
-            <p>Descripcion de la peluqueria</p>
+            <h2 className="pb-4 mb-4 border-bottom border-warning">Mavi's by dunamis</h2>
+            <p>Somos una peluqueria y estetica situada en Palma de Mallorca</p>
+            <p>Contamos con una Maestra peluquera con años de experiencia</p>
+            <p>Y una Masajista titulada especialista en masajes y tratamientos terapeuticos</p>
+            <p>Mejora tu belleza y cuida tu salud en nuextro centro</p>
+            <p></p>
           </div>
         </div>
       </div>
+      <div className="mt-5">
+        <div className="row align-items-center my-5 gy-2 flex-column flex-md-row text-center">
+          <div className="col">
+            <h2 className="pb-4 mb-4 border-bottom border-warning" >Majorica</h2>
+            <p>La perla del mediterráneo. Majorica nació en Mallorca en 1890.</p>
+            <p>Plata de Ley y perlas cultivadas de la mejor calidad</p>
+            <p>Descubre nuestra colección en colaboracion y compra el regalo ideal.</p>
+          </div>
+          <div className="col">
+            <img className="mw-100 shadow" src="/src/Images/pearl.jpg" />
+          </div>
+        </div>
+      </div>
+
+
       <div className="d-flex justify-content-center my-5">
         <h1>Galeria de fotos</h1>
       </div>
 
-      <div className="row justify-content-center align-items-center g-5">
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
-        </div>
-        <div className="col-auto imagebox">
-          <img src="https://via.placeholder.com/300" />
-          <span className="overlay-text">Descripcion foto</span>
+      <div className="row">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair01.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Peinado</span>
+          </div>
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair02.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Color y peinado</span>
+          </div>
         </div>
 
+        <div className="col-lg-4 mb-4 mb-lg-0">
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair03.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Alisado Keratina</span>
+          </div>
+
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair04.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Color, mechas y corte</span>
+          </div>
+        </div>
+
+        <div className="col-lg-4 mb-4 mb-lg-0">
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair05.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Corte y peinado</span>
+          </div>
+          <div className="imagebox">
+            <img
+              src="/src/Images/hair06.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt=""
+            />
+            <span className="overlay-text">Color y mechas</span>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,22 +1,32 @@
 import { NavLink } from "react-router-dom";
 import logo from "../Images/logo_cut.png";
+import { Collapse } from 'bootstrap'
+import { useEffect, useState } from "react";
 
 export default function Layout(props) {
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        const myCollapse = document.getElementById('navbarNavAltMarkup')
+        const bsCollapse = new Collapse(myCollapse, { toggle: false })
+        toggle ? bsCollapse.show() : bsCollapse.hide()
+    }, [toggle])
+
     return <div>
-        <div className="container-fluid d-flex justify-content-center fixed-top bg-white shadow">
-            <nav className="navbar navbar-expand-lg navbar-light bg-white ">
-                <a className="navbar-brand" href="#">
+        <div className="container-fluid d-flex justify-content-center fixed-top bg-white shadow ">
+            <nav className="navbar navbar-expand-md navbar-light bg-white justify-content-between ">
+                <NavLink className="navbar-brand" to="/">
                     <img className="mw-100" height={50} src={logo} alt="Logo" />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                </NavLink>
+                <button onClick={() => setToggle(toggle => !toggle)} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse ms-5" id="navbarNavAltMarkup">
                     <div className="navbar-nav fs-4">
-                        <NavLink className="nav-item nav-link mx-5" to="/">Inicio</NavLink>
-                        <NavLink className="nav-item nav-link mx-5" to="/peluqueria">Peluqueria</NavLink>
-                        <NavLink className="nav-item nav-link mx-5" to="/estetica">Estetica</NavLink>
-                        <NavLink className="nav-item nav-link mx-5" to="/contacto">Contacto</NavLink>
+                        <NavLink className="nav-item nav-link mx-2 mx-lg-5" to="/">Inicio</NavLink>
+                        <NavLink className="nav-item nav-link mx-2 mx-lg-5" to="/peluqueria">Peluqueria</NavLink>
+                        <NavLink className="nav-item nav-link mx-2 mx-lg-5" to="/estetica">Estetica</NavLink>
+                        <NavLink className="nav-item nav-link mx-2 mx-lg-5" to="/contacto">Contacto</NavLink>
                     </div>
                 </div>
             </nav>
